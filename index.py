67b3868278492f69e4edf8a21ba846d3c230502d
Mehds <oulmakki@gmail.com>
2018-07-21 20:52:31 +0200

    figured out basic static file serving. Had a wild dive into turning this project towards Angular

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return render_template('index.html')
