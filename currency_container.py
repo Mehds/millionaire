6b292f6ee32945f0ee875b43d2573bd9e3fbb81a
Mehds <oulmakki@gmail.com>
2018-07-28 12:02:56 +0200

    basic computation in currency container.py

---
+++ currency_container.py
@@ -0,0 +1,61 @@
+import json
+
+class CurrencyContainer(object):
+	"""Main interaction with my Exchange rate data"""
+	
+	def __init__(self):
+		with open('./data/complete_data.json') as f:
+			self.currencies = json.load(f)
+
+	def get_currency_shorthands():
+		return list(self.currencies)
+
+	# properties are name, countries, country_codes, rate
+	# get an array of tuples, matching the currency symbol to a given property
+	def map_currency_property(self, prop):
+		outp = []
+		for (k, v) in self.currencies.items():
+			outp.append((k, v.get(prop)))
+
+		return outp
+
+	def get_currency_properties(self, currency):
+		return currencies.get(currency)
+	
+	def get_countries(self, currency):
+		return self.currencies.get(currency).get('countries')
+	
+	def get_rate(self, currency):
+		return self.currencies.get(currency).get('rate')
+	
+	def get_subset(self, subset):
+		return {k: self.currencies[k] for k in subset}
+
+	def convert(self, amount, init_currency, target_currency):
+		# convert to euro, then convert from euro to target_currency
+		return amount / self.get_rate(init_currency) * self.get_rate(target_currency)
+
+	def get_millionaire(self, amount, init_currency):
+		return {k: self.currencies[k] for k in self.currencies if self.convert(amount, init_currency, k) > 1000000}
+
+	def get_billionaire(self, amount, init_currency):
+		return {k: self.currencies[k] for k in self.currencies if self.convert(amount, init_currency, k) > 1000000000}
+
+
+# test = CurrencyContainer()
+# l = 0
+# amount = 1
+# while l < 158:
+# 	x = test.get_millionaire(amount, 'EUR')
+
+# 	print(x)
+# 	l = len(x)
+# 	print(l)
+# 	print(amount)
+
+# 	amount *= 10
+
+# print('-----------------------------------------------------------')
+# print(test.currencies.keys() - x.keys())
+
+# print(len(test.currencies.keys()))
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+from currency_container import *
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -7,3 +8,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 @app.route("/")
 def hello():
 	return render_template('index.html')
+
+@app.route("/convert")
+def convert():
+	converter = CurrencyContainer();
+	return converter.get_currency_shorthands()
